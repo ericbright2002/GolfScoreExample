45f588ae87819e8836f1e91d0fee677f24771372
Eric Bright <ericbright2002@yahoo.com>
2022-07-28 15:34:05 -0400

    Initial Commit

---
+++ GolfScoreExample/GolfScoreExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  GolfScoreExampleApp.swift
+//  GolfScoreExample
+//
+//  Created by Eric Bright on 7/28/22.
+//
+
+import SwiftUI
+
+@main
+struct GolfScoreExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
